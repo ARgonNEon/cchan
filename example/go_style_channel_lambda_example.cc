8f269291acbaba979c40891e7d5856aba2823a1f
Arne Sachtler <arne.sachtler@dlr.de>
2017-08-25 18:15:32 +0200

    Add example with lambda thread

---
+++ example/go_style_channel_lambda_example.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <boost/thread.hpp>
+#include "../GoStyleChannel.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    chan<int> c(5);
+
+    boost::thread send([&]() {
+        for (int i = 0; i<100; ++i) {
+            c << i; // send to channel
+            cout << "send: " << i << endl;
+        }
+        c.close();
+    });
+
+    boost::thread receive([&]() {
+        int j = 0;
+        while(j << c) { //receive from channel
+            cout << "recv: " << j << endl;
+        }
+    });
+
+    receive.join();
+    return 0;
+}
