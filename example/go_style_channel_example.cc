67cf63e7fdb5bbcf532dddeb2164244d354a1da6
Arne Sachtler <arne.sachtler@dlr.de>
2017-08-25 18:09:50 +0200

    Add example

--- example/go_style_channel_example.cc
+++ example/go_style_channel_example.cc
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <boost/thread.hpp>
 #include "../GoStyleChannel.h"
 
 using namespace std;
 
+void sender(chan<int> &out) {
+    for (int i = 0; i<100; ++i) {
+        out <<  i; //send to channel
+        cout << "send: " << i << endl;
+    }
+    out.close();
+}
+
+void receiver(chan<int> &in) {
+    int j = 0;
+    while (j << in) { //receive from channel (false if channel closed)
+        cout << "recv: " << j << endl;
+    }
+}
+
 int main(int argc, char **argv) {
-    cout << "hi" << endl;
+    chan<int> c(5);
+    boost::thread s(sender, std::ref(c));
+    boost::thread r(receiver, std::ref(c));
+    r.join();
+    return 0;
 }
